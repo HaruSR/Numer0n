ce4c6779360013ccb5db56817a8933deec3bd70c
石川陽貴 <Haruki@Haruki-no-MacBook-Pro.local>
2017-02-26 01:28:49 +0900

    first commit

---
+++ Numer0n/Numer0n/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numer0n
+//
+//  Created by 石川陽貴 on 2017/02/26.
+//  Copyright © 2017年 石川陽貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
