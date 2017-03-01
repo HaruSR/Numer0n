49f63ca3e808df1eae7312e80f0e131d973a8fe7
石川陽貴 <Haruki@Haruki-no-MacBook-Pro.local>
2017-03-02 04:18:20 +0900

    textfield change

--- Numer0n/Numer0n/ViewController.swift
+++ Numer0n/Numer0n/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate {
+    var p1 = ""
+    var p2 = ""
+    var flag1 = 0
+    var flag2 = 0
+    
+    
+    
     @IBOutlet weak var p1TextF: UITextField!
     @IBOutlet weak var p2TextF: UITextField!
     @IBOutlet weak var p1Send: UIButton!
@@ -19,7 +26,10 @@ class ViewController: UIViewController,UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.fi
-    }
+        p1TextF.delegate = self
+        p1TextF.returnKeyType = .done
+        
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -27,8 +37,51 @@ class ViewController: UIViewController,UITextFieldDelegate {
     }
 
     @IBAction func p1SendAction(_ sender: Any) {
+        if flag1 == 0 {
+            p1 = textFieldDidChange(p1TextF.text!)
+//            p1 = p1TextF.text!
+            self.p1TextView.text.append("\(p1)\n")
+            
+        } else {
+            self.p1TextView.text.append(textFieldDidChange(p1TextF.text!)+"\n")
+            
+//          self.p1TextView.text = numeron(p1TextF.text!)
+        }
     }
     @IBAction func p2SendAction(_ sender: Any) {
+        
+    }
+    func p1TextFShouldReturn(_ textField: UITextField) -> Bool {
+        p1TextF.resignFirstResponder()
+        
+        return true
+    }
+    func p2TextFShouldReturn(_ textField: UITextField) -> Bool {
+        p2TextF.resignFirstResponder()
+        
+        return true
+    }
+//    deinit {
+//        NotificationCenter.default.removeObserver(self)
+//    }
+//    
+    
+    func textFieldDidChange(_ text:String) -> String {
+        
+        var teX:String = ""
+        
+            if text.characters.count == 3 {
+                flag1 = 1
+                teX = text.substring(to: text.index(text.startIndex, offsetBy: 3))
+            }else {
+                teX = "error input"
+        }
+        return String(teX)
     }
     
 }
+
+
+
+
+
